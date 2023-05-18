a4ff4224d1497a9e6515379c7a411e4aaea4b1f1
Unyxe <114250878+Unyxe@users.noreply.github.com>
2023-05-18 16:17:42 +0100

    Add files via upload

---
+++ LTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using SimpleImpersonation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTest
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username: ");
+            string username = Console.ReadLine();
+
+            var credentials = new UserCredentials("LUTC", username, ".");
+
+            while (true)
+            {
+                try
+                {
+                    Impersonation.RunAsUser(credentials, LogonType.Network, () =>
+                    {
+                    });
+                }
+                catch
+                {
+                    Console.WriteLine("Locked!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
